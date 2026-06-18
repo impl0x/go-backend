1e45f95f38282929752e23f0da2152d1682b276c
impl0x <implox.v1@gmail.com>
2026-06-18 18:51:39 +0530

    removed config requirement and instead returning a default config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-backend
 
 go 1.26.2
+
+require github.com/labstack/echo/v5 v5.2.1 // indirect
--- server/ratelimit/ratelimit.go
+++ server/ratelimit/ratelimit.go
@@ -7,29 +7,25 @@ import (
 	"net/http"
 )
 
-type Ratelimit struct {
-	StatusCode   uint16    // default to 429
+type RatelimitConfig struct {
+	StatusCode   int    // default to 429
 	ErrorMessage string // default to "Too many requests!"
 }
 
 // returns a middleware which implements the ratelimiter
-func (r *Ratelimit) NewRatelimiter(rl ratelimiters.Ratelimiter) core_types.Middleware {
-	// validation logic
-	if r.ErrorMessage == "" {
-		r.ErrorMessage = "Too many requests!"
-	} else if r.StatusCode == 0 {
-		if r.StatusCode > 599 {
-			panic("Status code cannot be greater than 599")
-		}
-		r.StatusCode = 429
+func NewRatelimiter(rl ratelimiters.Ratelimiter) (core_types.Middleware, *RatelimitConfig) {
+	newRatelimitConfig := RatelimitConfig{
+		StatusCode:   http.StatusTooManyRequests,
+		ErrorMessage: "Too many requests",
 	}
 
 	return helper.NewMiddleware(
-		func(next http.Handler, w http.ResponseWriter, r *http.Request){
-			if !rl.Allow(r){
-				// TODO: return a 429 and ErrorMessage in proper format
-			}
-			next.ServeHTTP(w,r)
-		},
-	)
+			func(next http.Handler, w http.ResponseWriter, r *http.Request) {
+				if !rl.Allow(r) {
+					// TODO: return a 429 and ErrorMessage in proper format
+				}
+				next.ServeHTTP(w, r)
+			},
+		),
+		&newRatelimitConfig
 }
