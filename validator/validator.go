6fcf3f595aa2b0f62880706f41d7573667a6d922
impl0x <implox.v1@gmail.com>
2026-06-28 22:08:01 +0530

    improved validation logic

--- validator/validator.go
+++ validator/validator.go
@@ -27,118 +27,209 @@ var NumTypes = []reflect.Kind{
 
 const validatorTag = "validate"
 
-type ValidationErrors struct {
-	
+type ValidationError interface {
+	error
+	JsonFormat() map[string]any
+}
+
+type UserError struct {
+	detail string
+}
+
+func (de *UserError) Error() string {
+	return de.detail
+}
+func (de *UserError) JsonFormat() map[string]any {
+	return map[string]any{
+		"message": de.detail,
+	}
+}
+
+func newUserError(detail string) *UserError {
+	return &UserError{
+		detail: detail,
+	}
+}
+
+type MissingFieldError struct {
+	detail      string
+	jsonDetails struct {
+		field string
+	}
+}
+
+func newMissingFieldError(field string) *MissingFieldError {
+	return &MissingFieldError{
+		detail:      fmt.Sprintf("field \"%v\" not found", field),
+		jsonDetails: struct{ field string }{field},
+	}
+}
+func (mfe *MissingFieldError) Error() string {
+	return mfe.detail
+}
+func (mfe *MissingFieldError) JsonFormat() map[string]any {
+	return map[string]any{
+		"message": "missing required field",
+		"field":   mfe.jsonDetails.field,
+	}
+}
+
+type TypeError struct {
+	detail      string
+	jsonDetails struct {
+		field    string
+		got      string
+		expected string
+	}
+}
+
+func newTypeError(got, expected, field string) *TypeError {
+	return &TypeError{
+		detail: fmt.Sprintf("field \"%v\" has incorrect type", field),
+		jsonDetails: struct {
+			field    string
+			got      string
+			expected string
+		}{field, got, expected},
+	}
+}
+func (te *TypeError) Error() string {
+	return te.detail
+}
+func (te *TypeError) JsonFormat() map[string]any {
+	return map[string]any{
+		"message":  "incorrect type provided",
+		"field":    te.jsonDetails.field,
+		"provided": te.jsonDetails.got,
+		"expected": te.jsonDetails.expected,
+	}
 }
 
 var emailRx = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9]*$`)
 var urlRx = regexp.MustCompile(`^https?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?::\d{1,5})?(?:\/[^\s]*)?$`)
-var ErrTypeError = errors.New("Wrong type")
-var ErrValidation = errors.New("Validation error")
-var ErrMissingField = errors.New("Missing field")
-var ErrInvalidStruct = errors.New("Invalid struct")
 
-func Validate(v any) error {
-	rv := reflect.ValueOf(v)
-	if rv.Kind() == reflect.Pointer {
+func Validate(v any) []ValidationError {
+	errs := []ValidationError{}
+	rv := reflect.ValueOf(v)          // stores the value
+	if rv.Kind() == reflect.Pointer { // if v is a pointer then we dereference it
 		rv = rv.Elem()
 	}
-	rt := reflect.TypeOf(v)
+	rt := reflect.TypeOf(v) // stores the type
 	if rt.Kind() != reflect.Struct {
-		return ErrInvalidStruct
+		errs = append(errs, newUserError("Not a struct"))
+		return errs // if not struct we immediately return an error
 	}
-	for i := 0; i < rv.NumField(); i++ {
-		v := rv.Field(i)
-		t := rt.Field(i)
+	for i := 0; i < rv.NumField(); i++ { // loops over all the fields of the struct
+		v := rv.Field(i) // value
+		t := rt.Field(i) // type
 		if !t.IsExported() {
-			continue
+			continue // if field isn't exported we skip it
 		}
 		tag, ok := t.Tag.Lookup(validatorTag)
 		if !ok {
-			continue
+			continue // if our tag isn't present we skip the field
 		}
-		requirements := strings.Split(tag, ",")
+		requirements := strings.Split(tag, ",") // ex: required,min=2,max=8. we split the requirements/rules
 
 		if ok := slices.Contains(requirements, "required"); ok {
-			if v.IsZero() {
-				return ErrMissingField
+			if v.IsZero() { // if theres a required tag and 54 field is initialized to its zero value
+				errs = append(errs, newMissingFieldError(t.Name)) // we store a missing field error
+				continue
 			}
 		} else {
-			if v.IsZero() {
+			if v.IsZero() { // if the field isn't required and its a zero value, we skip it
 				continue
 			}
 		}
-		kind := v.Kind()
-		for _, r := range requirements {
-			switch r {
-			case "email":
-				if kind != reflect.String {
-					return ErrTypeError
-				}
-				if !emailRx.MatchString(v.String()) {
-					return ErrValidation
-				}
-			case "url":
-				if kind != reflect.String {
-					return ErrTypeError
-				}
-				if !urlRx.MatchString(v.String()) {
-					return ErrValidation
-				}
-			case "required":
-				continue
-			default:
-				cons := strings.Split(r, "=")
-				if len(cons) != 2 {
-					return ErrValidation
-				}
-				rule := cons[0]
-				cods := cons[1]
-				switch rule {
-				case "min":
-					min, err := strconv.ParseFloat(cods, 64)
-					if err != nil {
-						return ErrTypeError
-					}
-					if kind == reflect.String {
-						val := len(v.String())
-						if val < int(min) {
-							return ErrValidation
-						}
-					} else if slices.Contains(NumTypes, kind) {
-						if v.Convert(reflect.TypeFor[float64]()).Float() < min {
-							return ErrValidation
-						}
-					} else {
-						return ErrTypeError
-					}
-				case "max":
-					max, err := strconv.ParseFloat(cods, 64)
-					if err != nil {
-						return ErrTypeError
-					}
-					if kind == reflect.String {
-						val := len(v.String())
-						if val > int(max) {
-							return ErrValidation
-						}
-					} else if slices.Contains(NumTypes, kind) {
-						if v.Convert(reflect.TypeFor[float64]()).Float() > max {
-							return ErrValidation
-						}
-					} else {
-						return ErrTypeError
-					}
-				case "oneof":
-					got := fmt.Sprintf("%v", v.Interface())
-					allowed := strings.Split(cods, " ")
-					if !slices.Contains(allowed, got) {
-						return ErrValidation
-					}
-				default:
-					return errors.New("idk")
-				}
+		kind := v.Kind() // storing the type, will be used a lot below
+
+		for _, s := range requirements {
+			err := validateRule(s, v, t, kind)
+			if err != nil {
+				errs = append(errs, err)
 			}
+		}
+	}
+	if len(errs) == 0 {
+		return nil
+	}
+	return errs
+}
+
+func matchRegexRule(rx *regexp.Regexp, v reflect.Value, kind reflect.Kind, fieldName string) ValidationError {
+	if kind != reflect.String {
+		return newTypeError(kind.String(), reflect.String.String(), fieldName)
+	}
+	if !rx.MatchString(v.String()) {
+		return nil
+	}
+	return nil
+}
+
+func validateRule(s string, v reflect.Value, t reflect.StructField, kind reflect.Kind) ValidationError {
+	switch s {
+	case "email":
+		err := matchRegexRule(emailRx, v, kind, t.Name)
+		if err != nil {
+			return err
+		}
+	case "url":
+		err := matchRegexRule(urlRx, v, kind, t.Name)
+		if err != nil {
+			return err
+		}
+	case "required":
+		return nil // we took care of it before looping, so we can skip now
+	default:
+		cons := strings.Split(s, "=")
+		if len(cons) != 2 {
+			return ErrValidation
+		}
+		rule := cons[0]
+		cods := cons[1]
+		switch rule {
+		case "min":
+			min, err := strconv.ParseFloat(cods, 64)
+			if err != nil {
+				return ErrTypeError
+			}
+			if kind == reflect.String {
+				val := len(v.String())
+				if val < int(min) {
+					return ErrValidation
+				}
+			} else if slices.Contains(NumTypes, kind) {
+				if v.Convert(reflect.TypeFor[float64]()).Float() < min {
+					return ErrValidation
+				}
+			} else {
+				return ErrTypeError
+			}
+		case "max":
+			max, err := strconv.ParseFloat(cods, 64)
+			if err != nil {
+				return ErrTypeError
+			}
+			if kind == reflect.String {
+				val := len(v.String())
+				if val > int(max) {
+					return ErrValidation
+				}
+			} else if slices.Contains(NumTypes, kind) {
+				if v.Convert(reflect.TypeFor[float64]()).Float() > max {
+					return ErrValidation
+				}
+			} else {
+				return ErrTypeError
+			}
+		case "oneof":
+			got := fmt.Sprintf("%v", v.Interface())
+			allowed := strings.Split(cods, " ")
+			if !slices.Contains(allowed, got) {
+				return ErrValidation
+			}
+		default:
+			return errors.New("idk")
 		}
 	}
 	return nil
